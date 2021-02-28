8c955e3ff1908b47bbea4ab08416422abe5a0f33
Johann Blake <johann.blake@external.sinnerschrader.com>
2021-02-28 17:58:50 +0100

    Added image support.

--- app/src/main/java/dev/wirespec/hopeforpaws/da/paging/PetsPagingDataSource.kt
+++ app/src/main/java/dev/wirespec/hopeforpaws/da/paging/PetsPagingDataSource.kt
@@ -9,21 +9,30 @@ import dev.wirespec.hopeforpaws.models.PetListItemInfo
 
 class PetsPagingDataSource(private val petAPIOptions: PetAPIOptions) : PagingSource<Int, PetListItemInfo>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PetListItemInfo> {
-        val startPos = params.key ?: 0
+        var startPos = params.key ?: 0
+        petAPIOptions.startPos = startPos * PetAPIConfig.PAGING_SIZE
 
         return try {
-            val pets = Repository.getPets(petAPIOptions)
+            var pets = Repository.getPets(petAPIOptions)
 
-            var nextStartPos: Int? = null
+//            var prevKey = if (startPos > 0) startPos - PetAPIConfig.PAGING_SIZE else null
+//            var nextKey = if (pets.isNotEmpty()) startPos + PetAPIConfig.PAGING_SIZE else null
+            var prevKey: Int? = null
+            var nextKey: Int? = null
 
-            if (pets.size == PetAPIConfig.PAGING_SIZE) {
-                nextStartPos = startPos + PetAPIConfig.PAGING_SIZE
+            if (startPos == 3) {
+                prevKey = 2
+                nextKey = null
+                pets = mutableListOf()
+            } else {
+                prevKey = if (startPos > 0) startPos - 1 else null // PetAPIConfig.PAGING_SIZE else null
+                nextKey = if (pets.isNotEmpty()) startPos + 1 else null // PetAPIConfig.PAGING_SIZE else null
             }
 
             LoadResult.Page(
                 data = pets,
-                prevKey = null,
-                nextKey = nextStartPos
+                prevKey = prevKey,
+                nextKey = nextKey
             )
         } catch (e: Exception) {
             LoadResult.Error(e)
--- app/src/main/java/dev/wirespec/hopeforpaws/ui/pets/MainActivity.kt
+++ app/src/main/java/dev/wirespec/hopeforpaws/ui/pets/MainActivity.kt
@@ -3,22 +3,30 @@ package dev.wirespec.hopeforpaws.ui.pets
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+import dev.chrisbanes.accompanist.coil.CoilImage
 import dev.wirespec.hopeforpaws.models.PetListItemInfo
 import dev.wirespec.hopeforpaws.ui.theme.HopeForPawsTheme
 import kotlinx.coroutines.flow.Flow
 
 class MainActivity : AppCompatActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             HopeForPawsTheme {
                 // A surface container using the 'background' color from the theme
@@ -30,28 +38,46 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+@ExperimentalFoundationApi
 @Composable
 fun PetsObs(vm: PetsViewModel = viewModel()) {
     PetsUI(vm.pets)
 }
 
+@ExperimentalFoundationApi
 @Composable
 fun PetsUI(pets: Flow<PagingData<PetListItemInfo>>) {
+
     val petItems = pets.collectAsLazyPagingItems()
 
-    LazyColumn {
-
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
         items(petItems) { pet ->
 
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .padding(bottom = 15.dp)
+            ) {
+                CoilImage(
+                    data = "https://storage.googleapis.com/wirespec.appspot.com/images/cats/" + pet!!.id + "-1.jpg",
+                    contentDescription = "Random cute yorkshire",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
-
     }
 }
 
+
+@ExperimentalFoundationApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HopeForPawsTheme {
-        PetsObs()
+        //PetsObs()
     }
 }
