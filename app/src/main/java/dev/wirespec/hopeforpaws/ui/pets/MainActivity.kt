c680e4df2ab41eb892b6c377e54796dbe7662ea0
Johann Blake <johann.blake@external.sinnerschrader.com>
2021-03-01 15:19:45 +0100

    Added cat name to grid cell.

--- app/src/main/java/dev/wirespec/hopeforpaws/ui/pets/MainActivity.kt
+++ app/src/main/java/dev/wirespec/hopeforpaws/ui/pets/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -64,16 +66,16 @@ fun PetsUI(pets: Flow<PagingData<PetListItemInfo>>) {
 }
 
 @Composable
-fun PetGridRow(pet: PetListItemInfo, petItems: LazyPagingItems<PetListItemInfo>, colWidth: Int) {
+fun PetGridRow(petListItem: PetListItemInfo, petItems: LazyPagingItems<PetListItemInfo>, colWidth: Int) {
     // If the item that is being requested appears in the first column, return it and all the
     // other items following it that make up the row.
 
-    var pos = (pet.position + 1) % 3
+    var pos = (petListItem.position + 1) % 3
 
     if (pos != 1)
         return
 
-    var lastItemPos = pet.position + 2
+    var lastItemPos = petListItem.position + 2
 
     if (lastItemPos > petItems.itemCount - 1)
         lastItemPos = petItems.itemCount - 1
@@ -85,14 +87,21 @@ fun PetGridRow(pet: PetListItemInfo, petItems: LazyPagingItems<PetListItemInfo>,
             .padding(0.dp)
 
     ) {
-        for (c in pet.position until lastItemPos + 1) {
-            CoilImage(
-                data = "https://storage.googleapis.com/wirespec.appspot.com/images/cats/" + petItems.get(c)!!.id + "-1.jpg",
-                contentDescription = "",
-                modifier = Modifier.requiredWidth(colWidth.dp),
-                fadeIn = true,
-                contentScale = ContentScale.Fit
-            )
+        for (c in petListItem.position until lastItemPos + 1) {
+            val pet = petItems.get(c)
+
+            Box {
+                CoilImage(
+                    data = "https://storage.googleapis.com/wirespec.appspot.com/images/cats/" + pet!!.id + "-1.jpg",
+                    contentDescription = "",
+                    modifier = Modifier.requiredWidth(colWidth.dp),
+                    fadeIn = true,
+                    contentScale = ContentScale.Fit
+                )
+                Row(modifier = Modifier.padding(top = 110.dp)) {
+                    Text(pet.name, modifier = Modifier.width(colWidth.dp).padding(start = 5.dp), color = Color.White)
+                }
+            }
         }
     }
 }
