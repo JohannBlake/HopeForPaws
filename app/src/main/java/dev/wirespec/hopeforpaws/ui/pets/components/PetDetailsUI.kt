36460fee64a3c7dcc57957c8f3ab6d6f5e6f2e8e
Johann Blake <johann.blake@external.sinnerschrader.com>
2021-03-02 09:58:03 +0100

    Began adding thumbnails to details screen.

--- app/src/main/java/dev/wirespec/hopeforpaws/ui/pets/components/PetDetailsUI.kt
+++ app/src/main/java/dev/wirespec/hopeforpaws/ui/pets/components/PetDetailsUI.kt
@@ -1,19 +1,28 @@
 package dev.wirespec.hopeforpaws.ui.pets.components
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.chrisbanes.accompanist.coil.CoilImage
+import dev.wirespec.hopeforpaws.da.web.PETS_LARGE_IMAGES_PATH
+import dev.wirespec.hopeforpaws.da.web.PETS_THUMBNAIL_IMAGES_PATH
+import dev.wirespec.hopeforpaws.models.PetListItemInfo
+import dev.wirespec.hopeforpaws.ui.pets.PetsViewModel
 
 @Composable
-fun PetDetailsUI() {
+fun PetDetailsUI(vm: PetsViewModel = viewModel()) {
+    val pet = vm.selectedPetItem.value as PetListItemInfo
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(verticalArrangement = Arrangement.Top, modifier = Modifier.padding(10.dp)) {
             Card(
@@ -23,8 +32,31 @@ fun PetDetailsUI() {
                     .verticalScroll(rememberScrollState())
                     .wrapContentHeight()
             ) {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    CoilImage(
+                        data = "$PETS_LARGE_IMAGES_PATH${pet.id}-1.jpg",
+                        contentDescription = "",
+                        modifier = Modifier.fillMaxWidth(),
+                        fadeIn = true,
+                        contentScale = ContentScale.Crop
+                    )
 
-                Text("Details go here")
+                    if (pet.imageCount > 1) {
+                        Row(modifier = Modifier.fillMaxWidth().requiredHeight(90.dp).padding(top = 2.dp).horizontalScroll(rememberScrollState()), horizontalArrangement = Arrangement.spacedBy(2.dp)) {
+                            for (i in 1 until pet.imageCount + 1) {
+                                CoilImage(
+                                    data = PETS_THUMBNAIL_IMAGES_PATH + pet.id + "-" + i + ".jpg",
+                                    contentDescription = "",
+                                    modifier = Modifier.requiredWidth(90.dp),
+                                    fadeIn = true,
+                                    contentScale = ContentScale.Crop
+                                )
+                            }
+                        }
+                    }
+                }
+
+
             }
         }
     }
