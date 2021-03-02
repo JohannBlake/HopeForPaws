21c9b89d73258482a1dee65f2446a4bfd80bd9b5
Johann Blake <johann.blake@external.sinnerschrader.com>
2021-03-02 08:16:19 +0100

    Updated model.

--- app/src/main/java/dev/wirespec/hopeforpaws/models/PetListItemInfo.kt
+++ app/src/main/java/dev/wirespec/hopeforpaws/models/PetListItemInfo.kt
@@ -8,9 +8,11 @@ data class PetListItemInfo (
     @PrimaryKey
     var id: Int = 0,
     var name: String = "",
-    var genderId: Int = 0,
-    var age: Int = 0,
-    var photoUrl: String = "",
-    var location: String = "",
+    var birthdate: String = "",
+    var gender: String = "",
+    var color: String = "",
+    var type: String = "",
+    var description: String = "",
+    var imageCount: Int = 1,
     var position: Int = 0
 )
